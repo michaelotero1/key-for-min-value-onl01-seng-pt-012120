1ae20f8af9c8489900331573e2cac61df9e83c23
michael otero <michaelotero1118@gmail.com>
2020-02-03 04:18:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,8 +8,7 @@ def key_for_min_value(name_hash)
 
   current_minimum_val = nil 
   current_key_assoc_w_min_val = nil 
-  #the starting value and key assignment is nil
-  #because we haven't gone through the hash yet
+  
   name_hash.each do |key, value|
   #now we're starting to iterate through the hash
   #and if the current_min_val is nil, we've just
